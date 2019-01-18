cf4b3c1e0616b97fea3dd25ae21d61559f9b9b7c
holbrook <wanghaikuo@gmail.com>
2019-01-18 16:34:15 +0800

    xxxx

---
+++ src/Tangle.Trading/IBroker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Tangle.Trading
+{
+    public interface IBroker
+    {
+        /// <summary>
+        /// 交易适配器支持的市场代码。
+        /// 一个适配器可以支持多个，比如INDX    1
+        ///    SGEX    1
+/// XSHE    1
+/// XSHG    1
+        /// </summary>
+        /// <value>The markets.</value>
+        string[] Markets { get; }
+    }
+}
