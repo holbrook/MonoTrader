ed15998a4c2a9075724c7d400893abdab3e9138b
holbrook <wanghaikuo@gmail.com>
2019-01-28 15:44:29 +0800

    xxx

--- src/Tangle.Trading/IBroker.cs
+++ src/Tangle.Trading/IBroker.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Tangle.Trading.Base;
+
 namespace Tangle.Trading
 {
     public interface IBroker
@@ -15,8 +17,26 @@ namespace Tangle.Trading
         /// XDCE表示大连商品交易所，
         /// XSGE表示上海期货交易所，
         /// XZCE表示郑州商品交易所。
+        /// exchId/Grantexchlist    市场代码
+                //0   沪A
+                //2   沪B
+                //1   深A
+                //3   深B
+                //4   深港通
+                //5   沪港通
+                //6   特A(新三板)
+                //F 中金所
+                //S 上期所
+                //D 大商所
+                //Z 郑商所
+                //X 沪期权
+                //Y 深期权
         /// </summary>
         /// <value>The markets.</value>
         string[] Markets { get; }
+
+        AccountBase Account { get; }
+
+        void SynchronizeAccount();
     }
 }
