9b03f0a59977fe766f003ad898398ab3548b50df
holbrook <wanghaikuo@gmail.com>
2019-01-28 16:49:33 +0800

    tt broker

--- src/Tangle.Trading/DemoBroker.cs
+++ src/Tangle.Trading/DemoBroker.cs
@@ -9,13 +9,18 @@ namespace Tangle.Trading
     [Export(typeof(IBroker))]
     [Broker(typeof(IStockBroker), new string[] { "XSHE", "XSHG", "NEEQ" })]
     [Broker(typeof(IFutureBroker), new string[] { "CCFX", "XDCE" , "XSGE", "XZCE" })]
-    public class DemoBroker : IBroker
+    public class DemoBroker : IBroker, IStockBroker, IFutureBroker
     {
         public AccountBase Account => throw new NotImplementedException();
 
+        public Stock.Order OrderShares(string orderbookID, int amount, OrderType style = null)
+        {
+            return null;
+        }
+
         public void SynchronizeAccount()
         {
-            throw new NotImplementedException();
+
         }
     }
 }
